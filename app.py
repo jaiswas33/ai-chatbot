5f54f0d0feff9bca4d0583278c5296d8295b24f8
Jaiswas33 <jaiswal.sudhanshu@gmail.com>
2025-05-18 23:34:32 +0530

    fix app.py

--- app.py
+++ app.py
@@ -28,4 +28,5 @@ def index():
     return render_template("index.html", response=response_text)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
